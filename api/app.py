c4d47763e8b00634dee8a08733ffbe4997f65a85
dkoerber <dkoerber@berkeley.edu>
2020-12-12 21:32:47 -0800

    add POST method support

--- api/app.py
+++ api/app.py
@@ -34,5 +34,22 @@ def get_beer(beer_id):
         flask.abort(404)
     return(flask.jsonify({'beer': beer[0]}))
 
+@app.route('/satnight/api/v1.0/beers', methods = ['POST'])
+def add_beer():
+    if not flask.request.json or not 'brand' in flask.request.json:
+        flask.abort(400)
+    beer = {
+        'id': beers[-1]['id'] + 1,
+        'brand': flask.request.json['brand'],
+        'description': flask.request.json.get('description', ""),
+        'drinkable': True,
+    }
+    beers.append(beer)
+    return(flask.jsonify({'beer': beer}), 201)
+
+@app.errorhandler(404)
+def not_found(error):
+    return(flask.make_response(flask.jsonify({'error': 'Not found'}), 404))
+
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 5000, debug = True)
