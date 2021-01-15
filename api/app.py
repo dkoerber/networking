d55fd6600ef72da2cdb6e3c2ddc903fd89c8a6a1
dkoerber <dkoerber@berkeley.edu>
2021-01-15 01:00:41 -0800

    add ipv6 and https support to flask app

--- api/app.py
+++ api/app.py
@@ -120,4 +120,9 @@ def not_found(error):
     return(f.make_response(f.jsonify({'error': 'Not found'}), 404))
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port = 5000, debug = True)
+    app.run(
+        host = '::1',
+        port = 5000,
+        debug = True,
+        ssl_context = ('cert.pem', 'key.pem'),
+    )
